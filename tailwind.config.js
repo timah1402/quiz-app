c5c26b9a2beda9b4568c3aa84b0e3658fc8dec87
emeamj@gmail.com <emeamj@gmail.com>
2024-12-08 19:37:33 +0000

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add all files in the src folder where Tailwind should look for classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
